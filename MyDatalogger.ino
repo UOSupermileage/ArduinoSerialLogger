06c6fdb44ec9ebe93e40ee5d2011a491f242e9b0
Jeremy Cote <jeremymcote@gmail.com>
2023-03-16 14:23:45 -0400

    Cleanup

--- MyDatalogger.ino
+++ MyDatalogger.ino
@@ -1,118 +1,127 @@
 /*
-  SD card datalogger
+  SD Serial Logger
 
-  This example shows how to log data from three analog sensors
-  to an SD card using the SD library.
+  Log messages incoming on Serial1 to an SD card.
+  We use the Arduino MKR GSM 1400, but other arduinos will also work.
 
-  The circuit:
-   analog sensors on analog ins 0, 1, and 2
-   SD card attached to SPI bus as follows:
- ** MOSI - pin 11
- ** MISO - pin 12
- ** CLK - pin 13
- ** CS - pin 4 (for MKRZero SD: SDCARD_SS_PIN)
-
-  created  24 Nov 2010
-  modified 9 Apr 2012
-  by Tom Igoe
-
-  This example code is in the public domain.
-
+  created  Mar 16 2023
+  by Jeremy Côté
 */
 
 #include <SPI.h>
 #include <SD.h>
 
 const int chipSelect = 21;
-const int logButton = 6;
-const int light = 5;
+const int logButton = 6; // Button to enable/disable logging
+const int light = 5; // Signal light
 
 File dataFile;
 bool fileMounted = false;
 
 void setup() {
-
+  // Setup pins
   pinMode(chipSelect, OUTPUT);
   pinMode(logButton, INPUT);
   pinMode(light, OUTPUT);
 
-
-  // Open serial communications and wait for port to open:
+  // Open serial communications
   Serial.begin(115200);
-  digitalWrite(light, HIGH);
-
   Serial1.begin(115200);
-
-  while (!Serial) {
-    ;
-  }
-
-  digitalWrite(light, LOW);
 
   Serial.println("UO Supermileage Datalogger");
 }
 
-void loop() {
+void mount() {
+  digitalWrite(light, HIGH);
 
+  Serial.print("Initializing SD card...");
+
+  // see if the card is present and can be initialized:
+  if (!SD.begin(chipSelect)) {
+    Serial.println("Card failed, or not present");
+      digitalWrite(light, LOW);
+      fileMounted = false;
+      return;
+  }
+
+  Serial.println("card initialized.");  
+
+  String filename = createFilename();
+
+  Serial.println("Opening file " + filename);
+  dataFile = SD.open(filename, FILE_WRITE);
+        
+  if (dataFile) {
+    // Successfully opened file
+    Serial.println("Mounted file");
+
+    // Create header
+    dataFile.println("id,value");
+    fileMounted = true;
+  } else {
+    Serial.println("Failed to open file");
+    fileMounted = false;
+    digitalWrite(light, LOW);
+  }    
+}
+
+void unmount() {
+
+  if (fileMounted) {
+    dataFile.close();
+    SD.end();
+  }
+  
+  fileMounted = false;
+  digitalWrite(light, LOW);
+}
+
+String createFilename() {
+  String filename = "logs00.CSV";
+  for (uint8_t i = 0; i < 100; i++) {
+    filename[4] = i/10 + '0';
+    filename[5] = i%10 + '0';
+    if (! SD.exists(filename)) {
+      break;  // leave the loop!
+    }
+  }
+  return filename;
+}
+
+void loop() {
+  // If button pressed (We use a switch)
   if (digitalRead(logButton) == HIGH) {
     if (fileMounted) {
       // if the file is available, write to it:
       if (dataFile) {
+        // If messages in Serial1
         if (Serial1.available() > 0) {
+          
+          // Turn on light to signal write in progress
           digitalWrite(light, HIGH);
+          
+          // Get line from serial
           String line = Serial1.readStringUntil('\n');
+          
+          // Write to SD Card
           dataFile.println(line);
-          // print to the serial port too:
+
+          // Print to Serial too
           Serial.println("\r\nWriting: " +line);
+
+          // Turn off light to signal write complete
           digitalWrite(light, LOW);
-        } else {
-          Serial.print("-");
         }
       }
       // if the file isn't open, pop up an error:
       else {
-        Serial.println("error opening datalog.csv");
+        Serial.println("error opening csv file...");
       }
     } else {
-      Serial.print("Initializing SD card...");
-
-      // see if the card is present and can be initialized:
-      if (!SD.begin(chipSelect)) {
-        Serial.println("Card failed, or not present");
-        // don't do anything more:
-        fileMounted = false;
-      } else {
-        Serial.println("card initialized.");  
-        Serial.println("Mounting file");
-
-        String filename = "logs00.CSV";
-        for (uint8_t i = 0; i < 100; i++) {
-          filename[4] = i/10 + '0';
-          filename[5] = i%10 + '0';
-          if (! SD.exists(filename)) {
-            break;  // leave the loop!
-          }
-        }
-
-        Serial.println("Saving to " + filename);
-        dataFile = SD.open(filename, FILE_WRITE);
-        
-        if (! dataFile) {
-          Serial.println("Failed to mount file");
-        } else {
-          Serial.println("Mounted file");
-          dataFile.println("id,value");
-
-          fileMounted = true;
-        }
-      }
+      mount();  
     }
   } else if (fileMounted) {
-      dataFile.close();
-      SD.end();
-      fileMounted = false;
-  } else {
-    Serial.println("Waiting...");
+      unmount();
   }
 }
 
